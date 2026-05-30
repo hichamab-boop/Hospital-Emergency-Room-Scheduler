ed4f746ba7c3ec5b18f25175bf75b20ee6dbc099
hichamab-boop <hichamabdoulkarim@gmail.com>
2026-05-30 10:47:04 +0300

    Implement PatientLookup class for patient management

---
+++ PatientLookup.java
@@ -0,0 +1,30 @@
+package structures;
+
+import models.Patient;
+import java.util.HashMap;
+
+public class PatientLookup {
+    private HashMap<Long, Patient> lookupTable;
+
+    public PatientLookup() {
+        this.lookupTable = new HashMap<>();
+    }
+
+    public void put(Patient p) {
+        if (p != null) {
+            lookupTable.put(p.getPatientID(), p);
+        }
+    }
+
+    public Patient get(long patientId) {
+        return lookupTable.get(patientId);
+    }
+
+    public boolean contains(long patientId) {
+        return lookupTable.containsKey(patientId);
+    }
+
+    public void remove(long patientId) {
+        lookupTable.remove(patientId);
+    }
+}
